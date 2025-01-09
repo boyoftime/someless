8bb8fccd429562fbc78d9aff2f5ffd88d509e661
Someless <150548091+boyoftime@users.noreply.github.com>
2025-01-09 15:51:23 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const softwareItems = [
         {
-            name: 'Hill Climb Racing',
-            image: 'images/hill climb.jpeg',
+            name: 'Hill Climb Racing PC Data',
+            image: 'images/hill climb.jpg',
             size: 0.59,
             description: JSON.stringify({
                 about: 'This data will help you get the most you want in Hill Climb Racing game with Unlimited Coins + Diamonds',
